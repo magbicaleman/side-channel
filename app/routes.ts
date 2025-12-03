2f9d0079d86b328db6433a691922ce0ef3a285f4
Magbic Aleman <mr.magbic@gmail.com>
2025-12-03 15:09:09 -0500

    fix: missing route to resource route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/_index.tsx"), route("r/:roomId", "routes/r.$roomId.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/_index.tsx"), 
+    route("r/:roomId", "routes/r.$roomId.tsx"),
+    route("api/room/:roomId/websocket", "routes/api.room.$roomId.websocket.ts"),
+] satisfies RouteConfig;
